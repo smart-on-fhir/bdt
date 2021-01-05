45ebc658663122a986c44a7b05dc0bbf9fa4dd7b
Vladimir Ignatov <vlad.ignatov@gmail.com>
2021-01-05 14:59:21 -0500

    Improved duration formatting

--- reporters/stdout.js
+++ reporters/stdout.js
@@ -69,36 +69,54 @@ function getColorForDuration(duration) {
     return "green";
 }
 
+/**
+ * Rounds the given number @n using the specified precision.
+ * @param {Number|String} n
+ * @param {Number} [precision]
+ * @param {Number} [fixed] The number of decimal units for fixed precision. For
+ *   example `roundToPrecision(2.1, 1, 3)` will produce `"2.100"`, while
+ *   `roundToPrecision(2.1, 3)` will produce `2.1`.
+ * @returns {Number|String} Returns a number, unless a fixed precision is used
+ */
+function roundToPrecision(n, precision, fixed) {
+    n = parseFloat(n + "");
+
+    if ( isNaN(n) || !isFinite(n) ) {
+        return NaN;
+    }
+
+    if ( !precision || isNaN(precision) || !isFinite(precision) || precision < 1 ) {
+        n = Math.round( n );
+    }
+    else {
+        const q = Math.pow(10, precision);
+        n = Math.round( n * q ) / q;
+    }
+
+    if (fixed) {
+        n = Number(n).toFixed(fixed);
+    }
+
+    return n;
+}
+
 function formatDuration(ms) {
-    let out = [];
     let meta = [
         { label: "week"  , n: 1000 * 60 * 60 * 24 * 7 },
         { label: "day"   , n: 1000 * 60 * 60 * 24     },
         { label: "hour"  , n: 1000 * 60 * 60          },
         { label: "minute", n: 1000 * 60               },
-        { label: "second", n: 1000                    },
-        { label: "m"     , n: 1                       }
+        { label: "second", n: 1000                    }
     ];
 
-    meta.reduce((prev, cur, i, all) => {
-        let chunk = Math.floor(prev / cur.n);
-        if (chunk) {
-            out.push(`${chunk} ${cur.label}${chunk > 1 ? "s" : ""}`);
-            return prev - chunk * cur.n
+    for (let cur of meta) {
+        let chunk = ms / cur.n;
+        if (chunk > 1) {
+            return `${roundToPrecision(chunk, 1)} ${cur.label}${chunk > 1 ? "s" : ""}`;
         }
-        return prev
-    }, ms);
-
-    if (!out.length) {
-        out.push(`0 ${meta.pop().label}s`);
     }
 
-    if (out.length > 1) {
-        let last = out.pop();
-        out[out.length - 1] += " and " + last;
-    }
-
-    return out.join(", ")
+    return `${ms} milliseconds`;
 }
 
 function duration(node) {
